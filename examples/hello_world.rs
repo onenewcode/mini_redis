2b9fb2d9328f7a4c4c06a47b2754caf24360f41b
alphonsezhang <alphonsezhang@xiaoe-tech.com>
2024-06-27 16:48:39 +0800

    理解hello_world

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,29 +1,14 @@
-//! Hello world server.
-//!
-//! A simple client that connects to a mini-redis server, sets key "hello" with value "world",
-//! and gets it from the server after
-//!
-//! You can test this out by running:
-//!
-//!     cargo run --bin mini-redis-server
-//!
-//! And then in another terminal run:
-//!
-//!     cargo run --example hello_world
-
-#![warn(rust_2018_idioms)]
-
 use mini_redis::{client, Result};
-
 #[tokio::main]
 pub async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
+    // 与mini-redis服务器建立连接
     let mut client = client::connect("127.0.0.1:6379").await?;
 
-    // Set the key "hello" with value "world"
+    // 客户端写入值
     client.set("hello", "world".into()).await?;
 
-    // Get key "hello"
+    // 获取值
+    // 如果键不存在，则返回None
     let result = client.get("hello").await?;
 
     println!("got value from the server; success={:?}", result.is_some());
