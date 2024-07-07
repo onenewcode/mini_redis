212ce217d0bb67e1e2c46b5c77d3495fc2d535e5
alphonsezhang <alphonsezhang@xiaoe-tech.com>
2024-07-07 19:06:28 +0800

    rm

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,18 +1,3 @@
-//! Hello world server.
-//!
-//! A simple client that connects to a mini-redis server, sets key "hello" with value "world",
-//! and gets it from the server after
-//!
-//! You can test this out by running:
-//!
-//!     cargo run --bin mini-redis-server
-//!
-//! And then in another terminal run:
-//!
-//!     cargo run --example hello_world
-
-#![warn(rust_2018_idioms)]
-
 use mini_redis::{clients::Client, Result};
 
 #[tokio::main]
