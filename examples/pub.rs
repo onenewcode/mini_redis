2f2d2d29aeb73bd0cb1eb1af757a0732a67b7571
alphonsezhang <alphonsezhang@xiaoe-tech.com>
2024-07-08 08:50:59 +0800

    rm

--- examples/pub.rs
+++ examples/pub.rs
@@ -1,19 +1,3 @@
-//! Publish to a redis channel example.
-//!
-//! A simple client that connects to a mini-redis server, and
-//! publishes a message on `foo` channel
-//!
-//! You can test this out by running:
-//!
-//!     cargo run --bin mini-redis-server
-//!
-//! Then in another terminal run:
-//!
-//!     cargo run --example sub
-//!
-//! And then in another terminal run:
-//!
-//!     cargo run --example pub
 
 #![warn(rust_2018_idioms)]
 
